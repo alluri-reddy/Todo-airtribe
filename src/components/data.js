5b8c3128e62af999f4382d9c39d1968c47fab6f3
alluri-reddy <alluri7700@gmail.com>
2024-03-10 16:30:01 +0530

    final commit

---
+++ src/components/data.js
@@ -0,0 +1,92 @@
+export const defaultCols = [
+    {
+      id: "Not Started",
+      title: "Not Started",
+      color: '#ffccd1'
+    },
+    {
+      id: "In Progress",
+      title: "In Progress",
+      color: '#fbeecc'
+    },
+    {
+      id: "Completed",
+      title: "Completed",
+      color: '#cce7e1'
+    },
+  ];
+  
+  export const defaultTasks=[
+    {
+      id: "1",
+      columnId: "Not Started",
+      title: "List admin APIs for dashboard",
+      description: "this is the description of the task id 1 ",
+    },
+    {
+      id: "2",
+      columnId: "Not Started",
+      title:"Develop user registration functionality with OTP number confirmation",
+      description: "this is the description of the task",
+    },
+    {
+      id: "3",
+      columnId: "In Progress",
+      title: "Conduct security testing",
+      description: "this is the description of the task",
+    },
+    {
+      id: "4",
+      columnId: "In Progress",
+      title: "Analyze competitors",
+      description: "this is the description of the task",
+    },
+    {
+      id: "5",
+      columnId: "Completed",
+      title: "Create UI kit documentation",
+      description: "this is the description of the task",
+    },
+    {
+      id: "6",
+      columnId: "Completed",
+      title: "Dev meeting",
+      description: "this is the description of the task",
+    },
+    {
+      id: "7",
+      columnId: "Completed",
+      title: "Deliver dashboard prototype",
+      description: "this is the description of the task",
+    },
+    {
+      id: "8",
+      columnId: "Not Started",
+      title: "Optimize application performance",
+      description: "this is the description of the task",
+    },
+    {
+      id: "9",
+      columnId: "Not Started",
+      title: "Implement data validation",
+      description: "this is the description of the task",
+    },
+    {
+      id: "10",
+      columnId: "Not Started",
+      title: "Design database schema",
+      description: "this is the description of the task",
+    },
+    {
+      id: "11",
+      columnId: "Not Started",
+      title: "Integrate SSL web certificates into workflow",
+      description: "this is the description of the task",
+    },
+    {
+      id: "12",
+      columnId: "In Progress",
+      title: "Implement error logging and monitoring",
+      description: "this is the description of the task",
+    },    
+  ];
