2c1b72ddb10e79fea529c849a4554b946a3afb85
alluri-reddy <alluri7700@gmail.com>
2024-03-12 12:43:56 +0530

    final

--- src/components/Task.js
+++ src/components/Task.js
@@ -45,7 +45,7 @@ function TaskCard({task}) {
   
 
   return (
-    <div onClick={handleClick} ref={setNodeRef} style={style} {...attributes} {...listeners}  className="bg-black p-3 h-[100px] min-h-[100px]  flex items-center text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 active:cursor-grabbing cursor-grab relative task overflow-x-hidden overflow-y-auto">
+    <div onClick={handleClick} ref={setNodeRef} style={style} {...attributes} {...listeners}  className="bg-white text-black p-3 h-[100px] min-h-[100px]  flex items-center text-left rounded-xl border-[2px] border-black  hover:ring-2 hover:ring-inset hover:ring-black active:cursor-grabbing cursor-grab relative task overflow-x-hidden overflow-y-auto">
       
       <p className="my-auto h-[90%] w-full overflow-y-hidden overflow-x-hidden whitespace-pre-wrap flex items-center justify-center">
         {task.title}
